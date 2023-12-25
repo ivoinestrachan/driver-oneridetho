341d8e2a8a5963a64efa8f95249917f12e4f9ca8
Ivoine <ivoinestrachan242@gmail.com>
2023-12-24 19:27:21 -0500

    prisma stuff

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,8 @@
 //@ts-nocheck
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+const prisma = new PrismaClient().$extends(withAccelerate())
 
 let prisma: PrismaClient;
 
